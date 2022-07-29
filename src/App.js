d0bdc008fb735753124b212dd6a20fa129aacea3
ashcruzzz <ashleycruzart@gmail.com>
2022-07-28 22:08:16 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
+      <h1>Dictionary</h1>
     </div>
   );
 }
