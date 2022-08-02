d3c628eb5567d01c703b94dfa2e3ef6ad32638db
ashcruzzz <ashleycruzart@gmail.com>
2022-08-02 13:47:13 -0700

    added event listener to search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by SheCodes</footer>
+      </div>
     </div>
   );
 }
