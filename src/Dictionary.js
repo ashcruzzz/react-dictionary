2f88cf4dfd5f10eea3726c4d1b31129de61bcb76
ashcruzzz <ashleycruzart@gmail.com>
2022-08-11 12:26:34 -0700

    added useState and added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
-
+    //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
@@ -26,6 +28,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      {keyword}
+      <Results results={results} />
     </div>
   );
 }
