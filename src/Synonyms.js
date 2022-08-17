46def8df860dafa26aa50b6c98fbecd6aa16fe5a
ashcruzzz <ashleycruzart@gmail.com>
2022-08-17 16:29:51 -0700

    changed to list format to show synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
     return (
       <div className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}</span>;
+          return <li key={index}>{synonym}</li>;
         })}
       </div>
     );
