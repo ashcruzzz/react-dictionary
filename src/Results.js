24bdf29c47dd6c00a0f40f45271f4339bccb7702
ashcruzzz <ashleycruzart@gmail.com>
2022-08-17 17:40:07 -0700

    added phonetic link to sound

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
+
 export default function Result(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
         <h1>{props.results.word}</h1>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
